aaefa039180fbb2511e102f70f4e62168258db28
Yiran <562751689@qq.com>
2019-06-09 14:13:45 +0800

    first commit

---
+++ precache-manifest.83157ce12183d61b140eb5000511d722.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60999953a6a3d578cc309e0d1db39a59",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2517de522ec5962cb60e",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "4683c7bee387cd86d791",
+    "url": "/static/js/2.f1142112.chunk.js"
+  },
+  {
+    "revision": "2517de522ec5962cb60e",
+    "url": "/static/js/main.78cbe1bb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8a7acbfd978f268527de07ab0753c69a",
+    "url": "/static/media/0.8a7acbfd.jpg"
+  },
+  {
+    "revision": "9537dade10260da8a3e1d1123860e75a",
+    "url": "/static/media/p1.9537dade.png"
+  },
+  {
+    "revision": "5594f32e662e223294ce2ca53b0614d5",
+    "url": "/static/media/p2.5594f32e.png"
+  },
+  {
+    "revision": "1644fad575a213493e42328ab5390f4c",
+    "url": "/static/media/p3.1644fad5.png"
+  },
+  {
+    "revision": "6b63c8fbfa921a8817f43aa2b4226ab6",
+    "url": "/static/media/p4.6b63c8fb.png"
+  }
+]);
